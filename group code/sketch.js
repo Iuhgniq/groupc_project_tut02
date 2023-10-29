7d6c66e5b858269163e5cccf4446a09274ad446f
Iuhgniq <qhua6277@uni.sydney.edu.au>
2023-10-30 01:04:05 +1100

    Sherry add variable in curves class and add color

--- group code/sketch.js
+++ group code/sketch.js
@@ -5,14 +5,29 @@ let moveOffset = 0;
 
 function setup() {
   createCanvas(1280, 720);
-  let col1 = color(255, 165, 0);
-  let col2 = color(255, 255, 0);
+  let colororange1 = color(199,69,18);//orange1
+  let colororange2 = color(195,95,55);//orange2
+  let colororange3 = color(220,159,117);//orange3
+  let colororange4 = color(221,139,45);//orange4
+  let colororange5 = color(181,124,79);//orange5
+  let colorgreen1 = color(106,128,107);//green1
+  let colorblue1 = color(17,23,32);//blackblue
+  let colorblue2 = color(35,64,67);//blue1
+  let colorblue3 = color(54,83,90);//blue2
+  let colorblue4 = color(97,126,129);//blue3
+  let colorblue5 = color(22,43,62);//blue4
+  
   //need more colour here
+  //curve = new flat/highCurve(横向位移，color，y，振幅数字越大振幅越大, 周期宽度数字越小周期越长)；
+
   noStroke();
-  curves[0] = new Curve(0, col1, 0, 30);
-  curves[1] = new Curve(0.1, col2, 50, 40);
-  curves[2] = new Curve(-0.1, col1, 60, 30);
-  curves[3] = new Curve(0.4, col2, 80, 60);
+  curves[0] = new Curve(0.5, colororange1, 0, 50, 0.05);
+
+  //curves[0] = new Curve(0, col1, 0, 30);
+  //curves[1] = new Curve(0.1, col2, 50, 40);
+  //curves[2] = new Curve(-0.1, col1, 60, 30);
+  //curves[3] = new Curve(0.4, col2, 80, 60);
+
   // Add more curves here
 }
 
@@ -45,12 +60,13 @@ function curve1(offset) {
 
 //this is the class of the curve
 class Curve {
-  constructor(start, fillColor, yOffset, amplitude) {
+  constructor(start, fillColor, yOffset, amplitude, xIncrement) {
     this.start = start;
     this.time = 0;
     this.fillColor = fillColor;
     this.yOffset = yOffset;
     this.amplitude = amplitude;
+    this.xIncrement = xIncrement;
   }
 
   display() {
@@ -60,11 +76,13 @@ class Curve {
       this.fillColor.levels[2]
     );
     let xoff = moveOffset + this.start;
+    let currentxIncrement = this.xIncrement;
+
     beginShape();
     for (let x = -width; x <= 2 * width; x += 10) {
       let y = map(sin(xoff), -1, 1, 0, this.amplitude);
       vertex(x, y + this.yOffset);
-      xoff += 0.1;
+      xoff += currentxIncrement;
     }
     vertex(width, height + this.yOffset);
     vertex(0, height + this.yOffset);
