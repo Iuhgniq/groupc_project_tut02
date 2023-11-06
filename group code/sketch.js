adc7d090633221302d40a0da2e49ecdf159b61c7
SilenceSinger <xzha0557@uni.sydney.edu.au>
2023-11-06 16:08:12 +1100

    change codes in ghost body functions

--- group code/sketch.js
+++ group code/sketch.js
@@ -13,8 +13,8 @@ function setup() {
 
 function draw() {
   background(220);
-
-  updateCurves();
+  noStroke();
+  drawCurves();
   drawBridge();
   drawPerson1();
   drawPerson2();
@@ -22,21 +22,19 @@ function draw() {
 
   image(ghostLayer, 0, 0);
 }
-function updateCurves(){
-  moveOffset += moveSpeed;
-
+//draw the background curves and the side curve(set the color of it)
+function drawCurves(){
   for (let i = 0; i < curves.length; i++) {
     curves[i].display();
   }
-
-  for (let xoff1 = 1200; xoff1 < width * 1.2; xoff1 += 50) {
-    let v = map(xoff1, 1200, width * 1.2, 0, 255);
-    fill(v, colOffset % 255, 200 - v);
-    curve1(xoff1);
+  for (let sidexoff = 1100; sidexoff < width * 1.3; sidexoff += 60) {
+    let change = map(sidexoff, 1200, width * 1.2, 15, 50);
+    fill(15, change + 10, change + 20);
+    sideCurve(sidexoff);
   }
 }
 
-// put all background curves together
+// set the background curves
 function backgroundCurve() {
   //background color
   let deeporange = color(163,44,12);
@@ -49,9 +47,8 @@ function backgroundCurve() {
   let deepblue = color(31,46,49);
   let midblue = color(49,83,85);
 
-  //curve = new flat/highCurve(横向位移，color，y，振幅数字越大振幅越大, 周期宽度数字越小周期越长)；
   noStroke();
-  //background orange
+  //background orange curves
   curves[0] = new Curve(0,backyellow,0,0,0);
   curves[1] = new Curve(0,midorange,-110,130,0.05);
   curves[2] = new Curve(0,backgreen,15,70,0.08);
@@ -65,7 +62,7 @@ function backgroundCurve() {
   curves[10] = new Curve(4.8,midorange,90,70,0.08);
   curves[11] = new Curve(4.8,yelloworange,105,70,0.08);
   
-  //background black blue - wave
+  //background black blue curves
   curves[12] = new Curve(0,deepblue,110,60,0.07);
   curves[13] = new Curve(2,midblue,170,80,0.06);
   curves[14] = new Curve(2,blackblue,200,70,0.06);
@@ -76,16 +73,17 @@ function backgroundCurve() {
   curves[19] = new Curve(2.5,deepblue,400,60,0.1);
   curves[20] = new Curve(2,blackblue,420,70,0.1);
   curves[21] = new Curve(0,midblue,520,60,0.09);
-  curves[22] = new Curve(0.5,blackblue,530,60,0.09);}
+  curves[22] = new Curve(0.5,blackblue,530,60,0.09);
+}
 
-//this is the side curve
-function curve1(offset) {
+//set the side curve
+function sideCurve(offset) {
   beginShape();
-  for (let y = height / 2; y < height * 1.1; y += 10) {
-    vertex(offset + 200 * sin(y * 0.01 - PI / 6), y);
+  for (let y = height / 3; y < height * 3; y += 10) {
+    vertex(offset + 200 * sin(y * 0.009 - PI / 6), y);
   }
-  vertex(width * 3.5 + offset, height * 1.1);
-  vertex(width * 3.5 + offset, height / 2);
+  vertex(width * 4 + offset, height * 1.1);
+  vertex(width * 4 + offset, height / 2);
   endShape(CLOSE);
 }
 
@@ -99,14 +97,13 @@ class Curve {
     this.amplitude = amplitude;
     this.xIncrement = xIncrement;
   }
-
   display() {
     fill(
       this.fillColor.levels[0],
       this.fillColor.levels[1],
       this.fillColor.levels[2]
     );
-    let xoff = moveOffset + this.start;
+    let xoff = this.start;
     let currentxIncrement = this.xIncrement;
 
     beginShape();
@@ -251,13 +248,13 @@ function drawGhostBodyWaves() {
     let nextX = (i + 1) * spacing;
     let fillColor = color(random(70, 100), 70, 50); 
     colours.push(fillColor);
-    drawGhostBody(waveWidth, 440, 1030, 500, fillColor, nextX, ghostLayer); 
+    drawGhostBody(waveWidth, 1030, 440, 500, fillColor, nextX, ghostLayer); 
   }
 }
 
 // draw curves of ghost body, by adding a new layer for the body
-// This function references the code in chatgpt, especially from line 265 to line 280.
-function drawGhostBody(waveWidth, startY, w, h, fillColor, nextX, ghostLayer) {
+// This function references the code in chatgpt
+function drawGhostBody(waveWidth, startX, startY, h, fillColor, nextX, ghostLayer) {
   let amplitude = 8;
   let frequency = 0.06;
 
@@ -271,58 +268,61 @@ function drawGhostBody(waveWidth, startY, w, h, fillColor, nextX, ghostLayer) {
   for (let i = 0; i <= h; i += 10) {
     let yOffset = i;
     let xOffset = sin(frequency * yOffset) * amplitude;
-    ghostLayer.vertex(waveWidth + w / 2 + xOffset, startY + yOffset);
+    ghostLayer.vertex(waveWidth + startX / 2 + xOffset, startY + yOffset);
   }
 
-  ghostLayer.vertex(waveWidth + w, startY + h); // end point
-  ghostLayer.vertex(nextX + w, startY + h); // connect to next curve
+  ghostLayer.vertex(waveWidth + startX, startY + h); // end point
+  ghostLayer.vertex(nextX + startX, startY + h); // connect to next curve
   for (let i = h; i >= 0; i -= 10) {
     let yOffset = i;
     let xOffset = sin(frequency * yOffset) * amplitude;
-    ghostLayer.vertex(nextX + w / 2 + xOffset, startY + yOffset);
+    ghostLayer.vertex(nextX + startX / 2 + xOffset, startY + yOffset);
   }
 
   ghostLayer.endShape(CLOSE);
 }
 
 
-
-function drawBridge(){
-    fill(120,88,24);
+  function drawBridge() {
+    fill(120, 88, 24); 
     stroke(0);
     strokeWeight(3);
 
-    // draw three black quads on the left
-    fill(120,88,24);
-    quad(73,199,0,380,0,480,80,199); //railing 3
-    //railing 2
-    quad(71.5,199.5,0,290,0,310,82.5,198.5);
-    quad(60,197,0,230,0,250,72,197); //railing 1
-    
-  // the desk of the bridge
-  fill(92, 63, 7);
-  noStroke();
-  quad(78,197,-80,720,477,720,147,197);
-    
-  // draw three black quads as shadows on the right
-  fill(0);
-  quad(147,197,477,720,597,720,154,197); //shadow of railing1
-  quad(157,197,667,720,797,720,162,197); //shadow of railing2
-  quad(167,197,947,720,1077,720,170,197); //shadow of railing3
+    // draw three horizontal railings on the left
+    // quad1
+    fill(70, 50, 10); 
+    quad(73, 197, 0, 380, 0, 480, 80, 199);
 
- // draw three khaki quads as railings on the right
-  fill(120,88,24);
-  quad(154,197,557,720,597,720,160,197); //railing1
-  quad(162,197,777,720,837,720,168,197); //railing2
-  quad(170,197,1037,720,1107,720,178,197); //railing3
+    // quad2
+    quad(69.5, 197, 0, 290, 0, 310, 82.5, 198.5);
+
+    // quad3
+    quad(60, 197, 0, 230, 0, 250, 72, 197);
   
-  // vertical handrail
-  //one on the left
-  quad(20,232,20,338,30,330,30,226);
-  //four on the right
-  quad(220,222,220,280,230,292,230,228);
-  quad(320,278.5,320,410,340,430,340,295);
-  quad(470,370,470,600,500,620,500,380);
-  quad(760,540,760,720,810,720,810,580);
+    // the desk of the bridge
+    fill(120, 88, 24);
+    quad(82, 197, -68, 720, 477, 720, 147, 197);
 
-  }
+     // shadows of right railings
+    fill(0); 
+    quad(147, 197, 477, 720, 597, 720, 154, 197);
+    quad(157, 197, 667, 720, 797, 720, 162, 197);
+    quad(167, 197, 947, 720, 1077, 720, 170, 197);
+
+    // horizontal railings on the right
+    fill(120, 88, 24); 
+    quad(154, 197, 557, 720, 597, 720, 160, 197);
+    quad(162, 197, 777, 720, 837, 720, 168, 197);
+    quad(170, 197, 1037, 720, 1107, 720, 178, 197);
+
+    // vertical railing on the left
+    fill(70, 50, 10); 
+    quad(20, 232, 20, 338, 30, 330, 30, 226);
+
+    // vertical railings on the right
+    fill(0); 
+    quad(220, 232, 220, 290, 230, 302, 230, 238);
+    quad(320, 288.5, 320, 420, 340, 440, 340, 305);
+    quad(470, 380, 470, 620, 500, 650, 500, 400);
+    quad(760, 560, 760, 720, 810, 720, 810, 590);
+}
